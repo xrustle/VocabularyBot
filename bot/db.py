d675351a1e80b0c9fe7c4452787d0114cba526c2
Dmitry <31348332+xrustle@users.noreply.github.com>
2020-01-30 19:08:42 +0300

    Refactoring + simple search engine

--- bot/db.py
+++ bot/db.py
@@ -7,43 +7,13 @@ class MongoDB:
         client = MongoClient(**MONGO['uri'])
         self.db = client.get_database(MONGO['db'])
 
-    def insert_user(self, record, coll_name='test'):
-        collection = self.db[coll_name]
-        collection.insert_one(record)
-
-    def is_user_in_db(self, user_id):
-        collection = self.db['users']
-        ret = True if collection.find_one({'_id': user_id}) else False
-        return ret
-
-    def add_update_user(self, user):
-        collection = self.db['users']
-        collection.update_one({'_id': user['_id']},
-                              {'$set': user},
-                              upsert=True)
-
     def insert_word(self, user_id, word):
         collection = self.db[str(user_id)]
         collection.insert_one(word)
 
-    def get_user(self, user_id):
-        collection = self.db['users']
-        user = collection.find_one({'_id': user_id})
-        return user
-
-    def set_user(self, user_id, step=0, word=None, message=None):
-        collection = self.db['users']
-        collection.update_one({'_id': user_id},
-                              {'$set': {'step': step,
-                                        'word': word}})
-        if message == 'cleanup':
-            collection.update_one({'_id': user_id},
-                                  {'$set': {'messages': []}})
-        elif message:
-            collection.update_one({'_id': user_id},
-                                  {'$push': {'messages': message}},
-                                  upsert=True)
-
+    def get_words(self, user_id):
+        collection = self.db[str(user_id)]
+        return collection.find({})
 
 db = MongoDB()
 
--- bot/main.py
+++ bot/main.py
@@ -2,77 +2,90 @@ from bot.config import TOKEN, PROXY
 from bot.msg import msg
 from bot.db import db
 import telebot
-from telebot import apihelper
+from telebot import apihelper, types
 
-# telebot.apihelper.proxy = PROXY
+apihelper.proxy = PROXY
 bot = telebot.TeleBot(TOKEN)
+
 users = {}
 
+find_markup = types.InlineKeyboardMarkup(row_width=1)
+find_markup.add(types.InlineKeyboardButton(text=msg('ru', 'find_button'), switch_inline_query_current_chat=''))
 
-def delete_messages(chat_id, msg_list):
-    for message in msg_list:
-        try:
-            apihelper.delete_message(TOKEN, chat_id, message)
-        except apihelper.ApiException:
-            pass
+add_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+add_markup.add(types.KeyboardButton(text=msg('ru', 'add_button')))
+
+clear_markup = types.ReplyKeyboardRemove(selective=False)
+
+
+def step_equals(user_id, step):
+    user = users.get(user_id)
+    if user:
+        return user['step'] == step
 
 
 @bot.message_handler(commands=['start'])
 def command_start(m: telebot.types.Message):
     cid = m.chat.id
-    if db.is_user_in_db(cid):
-        bot.send_message(cid, msg('ru', 'already_in'))
-    else:
-        db.add_update_user({'_id': cid, 'step': 0, 'messages': []})
-        bot.send_message(cid, msg('ru', 'user_added'))
+    bot.send_message(cid, text=msg('ru', 'start_instruction'), reply_markup=add_markup)
+    if cid not in users:
+        users[cid] = {'step': 0}
+
+
+@bot.message_handler(func=lambda m: m.text == msg('ru', 'add_button'))
+def command_add_ru(m: telebot.types.Message):
+    command_add(m)
 
 
 @bot.message_handler(commands=['add'])
-def command_start(m: telebot.types.Message):
+def command_add(m: telebot.types.Message):
     cid = m.chat.id
-    if not db.is_user_in_db(cid):
-        command_start(m)
-    message = bot.send_message(cid, msg('ru', 'insert_word'))
-    db.set_user(cid, 1, message=m.message_id)
-    db.set_user(cid, 1, message=message.message_id)
+    users[cid] = {'step': 1}
+    bot.send_message(cid, msg('ru', 'insert_word'), reply_markup=clear_markup)
 
 
-@bot.message_handler(func=lambda message: db.get_user(message.chat.id)['step'] == 1)
+@bot.message_handler(func=lambda m: step_equals(m.chat.id, 1))
 def command_start(m: telebot.types.Message):
     cid = m.chat.id
     if not m.text:
-        db.set_user(cid, 1, message=m.message_id)
-        message = bot.send_message(cid, msg('ru', 'waiting_for_text'))
-        db.set_user(cid, 1, message=message.message_id)
+        bot.send_message(cid, msg('ru', 'waiting_for_text'))
         return
-    message = bot.send_message(cid, msg('ru', 'insert_translate'))
-    db.add_update_user({'_id': cid})
-    db.set_user(cid, 2, word=m.text, message=m.message_id)
-    db.set_user(cid, 2, word=m.text, message=message.message_id)
+    bot.send_message(cid, msg('ru', 'insert_translate'))
+    users[cid] = {'step': 2,
+                  'word': m.text}
 
 
-@bot.message_handler(func=lambda message: db.get_user(message.chat.id)['step'] == 2)
+@bot.message_handler(func=lambda m: step_equals(m.chat.id, 2))
 def command_start(m: telebot.types.Message):
     cid = m.chat.id
     if not m.text:
-        db.set_user(cid, 2, message=m.message_id)
-        message = bot.send_message(cid, msg('ru', 'waiting_for_text'))
-        db.set_user(cid, 2, message=message.message_id)
+        bot.send_message(cid, msg('ru', 'waiting_for_text'))
         return
-    word = db.get_user(cid)['word']
-    db.insert_word(cid, {'word': word,
+    db.insert_word(cid, {'word': users[cid]['word'],
                          'definition': m.text})
-    bot.send_message(cid, msg('ru', 'word_was_inserted'))
-    db.set_user(cid, 0, message=m.message_id)
-    delete_messages(cid, db.get_user(cid)['messages'])
-    db.set_user(cid, message='cleanup')
-    bot.send_message(cid, word + ' - ' + m.text)
+    bot.send_message(cid,
+                     msg('ru', 'word_was_inserted') + '\n\n*' + users[cid]['word'] + '* - ' + m.text,
+                     parse_mode='markdown',
+                     reply_markup=add_markup)
+    users[cid] = {'step': 0}
 
 
-@bot.message_handler(commands=['show'])
-def command_start(m: telebot.types.Message):
-    cid = m.chat.id
-    bot.send_message(cid, msg('ru', 'start'))
+@bot.inline_handler(func=lambda _: True)
+def query_text(query):
+    # func=lambda query: len(query.query) > 0
+    # https://groosha.gitbook.io/telegram-bot-lessons/chapter7
+
+    words = db.get_words(query.from_user.id)
+
+    answer_list = [
+        types.InlineQueryResultArticle(
+            id=str(i), title=word['word'], description=word['definition'],
+            input_message_content=types.InputTextMessageContent(
+                message_text=f'*{word["word"]}* - {word["definition"]}', parse_mode='markdown')
+        )
+        for i, word in enumerate(words)
+    ]
+    bot.answer_inline_query(query.id, answer_list, cache_time=1)
 
 
 if __name__ == "__main__":
